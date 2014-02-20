12ede640b5c8db71d634893b1b298aee25288a18
Tamino Hartmann <tamino.hartmann@gmail.com>
2014-02-20 16:09:55 +0100

    stuff

--- src/database/objects/User.java
+++ src/database/objects/User.java
@@ -2,8 +2,21 @@ package database.objects;
 
 /**
  * Created by tamino on 2/19/14.
- *
+ * <p/>
  * zB placeholder user class
  */
 public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/servlet/Servlet.java
+++ src/servlet/Servlet.java
@@ -5,15 +5,15 @@
 package servlet;
 
 import com.google.gson.Gson;
+import database.objects.Error;
 import logger.Messenger;
-
-import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Dieses Servlet ist fuer alle oeffentlich zugaengliche Daten zustaendig.
@@ -25,6 +25,10 @@ import javax.servlet.http.HttpServletResponse;
 public class Servlet extends HttpServlet {
 
     /**
+     * TAG for logging.
+     */
+    private final String TAG = "Servlet";
+    /**
      * JSON library.
      */
     private Gson gson;
@@ -33,53 +37,69 @@ public class Servlet extends HttpServlet {
      */
     private Messenger log;
     private SanitationSecurity sanSec;
-    /**
-     * TAG for logging.
-     */
-    private final String TAG = "Servlet";
 
-	public Servlet() {
-		super();
+    public Servlet() {
+        super();
         gson = new Gson();
         log = Messenger.getInstance();
         sanSec = SanitationSecurity.getInstance();
-	}
+    }
 
     /**
      * This method receives all json requests that will result in a write or modify of objects.
+     *
      * @param request
      * @param response
      * @throws ServletException
      * @throws IOException
      */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		String path = request.getPathInfo();
-		path = (path == null) ? "" : path;
-        // todo
-        sanSec.secure();
-		switch (path) {
-            default:
-                log.log(TAG, "Unknown path sent: "+path);
-                break;
-        }
-	}
-
-    /**
-     * This method handles all simple data requests.
-     * @param request
-     * @param response
-     */
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
         String path = request.getPathInfo();
         path = (path == null) ? "" : path;
         // todo
         sanSec.secure();
+        // Write whatever you want sent back to this object:
+        Object answer = null;
         switch (path) {
             default:
-                log.log(TAG, "Unknown path sent: "+path);
+                log.log(TAG, "Unknown path sent: " + path);
+                answer = new Error("404", "Unknown path: <" + path + ">");
                 break;
         }
-	}
+        // If this happens, send back a standard error message.
+        if (answer == null) {
+            answer = new Error("NULL PUSH", "Your PUSH did not return an object!");
+        }
+        response.getWriter().write(gson.toJson(answer, answer.getClass()));
+        response.setContentType("application/json");
+    }
+
+    /**
+     * This method handles all simple data requests.
+     *
+     * @param request
+     * @param response
+     */
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws IOException {
+        String path = request.getPathInfo();
+        path = (path == null) ? "" : path;
+        // todo
+        sanSec.secure();
+        // Write whatever you want sent back to this object:
+        Object answer = null;
+        switch (path) {
+            default:
+                log.log(TAG, "Unknown path sent: " + path);
+                answer = new Error("404", "Unknown path: <" + path + ">");
+                break;
+        }
+        // If this happens, send back a standard error message.
+        if (answer == null) {
+            answer = new Error("NULL GET", "Your GET did not return an object!");
+        }
+        response.getWriter().write(gson.toJson(answer, answer.getClass()));
+        response.setContentType("application/json");
+    }
 }
