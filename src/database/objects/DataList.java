a7c348f680a8a2456871ccf8e8b77caa41505009
Andreas <andreas.koell@uni-ulm.de>
2014-02-25 11:01:20 +0100

    DataList

---
+++ src/database/objects/DataList.java
@@ -0,0 +1,12 @@
+package database.objects;
+
+import database.Data;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Cassio on 25.02.14.
+ */
+public class DataList extends ArrayList<Data> implements Data {
+
+}
