5a6bb6537ae7d10edef88c87c5747930aa7cbba7
Andreas <andreas.koell@uni-ulm.de>
2014-05-17 16:29:21 +0200

    nexus 5 in own class

--- src/de/uulm/mi/mind/objects/enums/DeviceClass.java
+++ src/de/uulm/mi/mind/objects/enums/DeviceClass.java
@@ -7,16 +7,18 @@ import java.util.Arrays;
  */
 public enum DeviceClass {
 
-    CLASS1, CLASS2, UNKNOWN;
+    CLASS1, CLASS2, CLASS3, UNKNOWN;
 
     private static final String[] class1 = new String[]{"CLASS1", "SONY LT25I", "SONY C2105", "LGE LG-D802"};
-    private static final String[] class2 = new String[]{"CLASS2", "SAMSUNG GT-I8190", "LGE NEXUS 5"};
+    private static final String[] class2 = new String[]{"CLASS2", "SAMSUNG GT-I8190"};
+    private static final String[] class3 = new String[]{"CLASS3", "LGE NEXUS 5"};
     private static final String[] simulated = new String[]{};
 
     public static DeviceClass getClass(String requestDeviceModel) {
         if (requestDeviceModel == null) return UNKNOWN;
         if (Arrays.asList(class1).contains(requestDeviceModel.toUpperCase())) return CLASS1;
         else if (Arrays.asList(class2).contains(requestDeviceModel.toUpperCase())) return CLASS2;
+        else if (Arrays.asList(class3).contains(requestDeviceModel.toUpperCase())) return CLASS3;
         else return UNKNOWN;
     }
 
