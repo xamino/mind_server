57cb8ee02a0f6dc2984a6a7861a35ae304e41af5
Tamino Hartmann <tamino.hartmann@gmail.com>
2014-07-15 17:01:39 +0200

    Added generic external FileLog access via FileLogObject and task "log".
    Did not remove PositionOkay|Error tasks, but might be up for discussion
    in the future.

---
+++ src/de/uulm/mi/mind/objects/FileLogObject.java
@@ -0,0 +1,35 @@
+package de.uulm.mi.mind.objects;
+
+import de.uulm.mi.mind.logger.permanent.LogObject;
+import de.uulm.mi.mind.objects.Interfaces.Sendable;
+
+/**
+ * Created by Tamino Hartmann on 7/15/14.
+ * Object used to send a log to the server.
+ */
+public class FileLogObject implements Sendable, LogObject {
+
+    // already in use elsewhere, here for convenience
+    private final static String SESSIONFILE = "session", POSITIONFILE = "position", POLLFILE = "polling", IPFILE = "ips";
+
+    private String file;
+    private String message;
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getFileName() {
+        return file;
+    }
+
+    @Override
+    public String getContent() {
+        return message;
+    }
+}
---
+++ src/de/uulm/mi/mind/tasks/multiple/LogTask.java
@@ -0,0 +1,66 @@
+package de.uulm.mi.mind.tasks.multiple;
+
+import de.uulm.mi.mind.logger.permanent.FileLog;
+import de.uulm.mi.mind.logger.permanent.LogObject;
+import de.uulm.mi.mind.logger.permanent.LogWorker;
+import de.uulm.mi.mind.objects.FileLogObject;
+import de.uulm.mi.mind.objects.Interfaces.Sendable;
+import de.uulm.mi.mind.objects.PublicDisplay;
+import de.uulm.mi.mind.objects.User;
+import de.uulm.mi.mind.objects.messages.Success;
+import de.uulm.mi.mind.security.Active;
+import de.uulm.mi.mind.tasks.Task;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Tamino Hartmann on 7/15/14.
+ * Allows clients to write to the server FileLog.
+ */
+public class LogTask extends Task<FileLogObject, Sendable> {
+    @Override
+    public boolean validateInput(FileLogObject object) {
+        return object.getFileName() != null && !object.getFileName().isEmpty() && object.getContent() != null &&
+                !object.getContent().isEmpty();
+    }
+
+    @Override
+    public Sendable doWork(Active active, final FileLogObject object, boolean compact) {
+        FileLog.getInstance().log(new LogWorker() {
+            @Override
+            public LogObject logCreate() {
+                return object;
+            }
+        });
+        return new Success("Logged.");
+    }
+
+    @Override
+    public String getTaskName() {
+        return "log";
+    }
+
+    @Override
+    public Set<String> getTaskPermission() {
+        Set<String> permissible = new HashSet<>();
+        permissible.add(PublicDisplay.class.getSimpleName());
+        permissible.add(User.class.getSimpleName());
+        return permissible;
+    }
+
+    @Override
+    public Class<FileLogObject> getInputType() {
+        return FileLogObject.class;
+    }
+
+    @Override
+    public Class<Sendable> getOutputType() {
+        return Sendable.class;
+    }
+
+    @Override
+    public boolean isAdminTask() {
+        return false;
+    }
+}
