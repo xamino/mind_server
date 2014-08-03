7a23917ff23a1a963872674315e7441e6e6476bb
Tamino Hartmann <tamino.hartmann@gmail.com>
2014-08-03 18:26:09 +0200

    Fixed Anonymizer ID bug, added unique_reset task
    Anonymizer wasn't creating hashes anymore, fixed that. Also added a method
    for reseting the unique fields for all users so that we can start a new
    clean evaluation round two without modifying the database.

--- src/de/uulm/mi/mind/tasks/admin/ResetUnique.java
+++ src/de/uulm/mi/mind/tasks/admin/ResetUnique.java
@@ -1,7 +1,62 @@
 package de.uulm.mi.mind.tasks.admin;
 
+import de.uulm.mi.mind.io.Session;
+import de.uulm.mi.mind.io.Transaction;
+import de.uulm.mi.mind.logger.anonymizer.Anonymizer;
+import de.uulm.mi.mind.objects.DataList;
+import de.uulm.mi.mind.objects.Interfaces.Data;
+import de.uulm.mi.mind.objects.None;
+import de.uulm.mi.mind.objects.User;
+import de.uulm.mi.mind.objects.messages.Information;
+import de.uulm.mi.mind.objects.messages.Success;
+import de.uulm.mi.mind.security.Active;
+import de.uulm.mi.mind.tasks.AdminTask;
+
 /**
  * Created by Tamino Hartmann on 8/3/14.
+ * <p/>
+ * This task resets all unique IDs in the database for User objects. This is mainly useful for restarting a new logging
+ * round with new unique anonymous IDs.
  */
-public class ResetUnique {
+public class ResetUnique extends AdminTask<None, Information> {
+    @Override
+    public boolean validateInput(None object) {
+        return true;
+    }
+
+    @Override
+    public Information doWork(Active active, None object, boolean compact) {
+        return ((Information) database.open(new Transaction() {
+            @Override
+            public Data doOperations(Session session) {
+                // read all users, max depth because otherwise we loose data!
+                DataList<User> users = database.read(new User(null), 11);
+                for (final User user : users) {
+                    // reset unique
+                    user.setUnique(null);
+                    if (!session.update(user)) {
+                        log.error(TAG, "Failed to reset unique for user " + user.getKey() + "!");
+                    }
+                }
+                // finally we MUST reset Anonymizer!
+                Anonymizer.getInstance().resetCache();
+                return new Success("Successfully reset all User's <unique> field.");
+            }
+        }));
+    }
+
+    @Override
+    public String getTaskName() {
+        return "reset_unique";
+    }
+
+    @Override
+    public Class<None> getInputType() {
+        return None.class;
+    }
+
+    @Override
+    public Class<Information> getOutputType() {
+        return Information.class;
+    }
 }
